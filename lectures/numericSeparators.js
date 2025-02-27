0574cf572ba93bff832b905feb45746114715d18
mmilushev91 <milushev_1990@abv.bg>
2025-02-27 21:34:51 +0200

    Create numericSeparators.js

---
+++ lectures/numericSeparators.js
@@ -0,0 +1,16 @@
+//Numeric separators
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+//returns NAN
+console.log(Number('230_000'));
